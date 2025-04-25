76660eeffbaed53a14ba058c33b98b8ad62efbae
Cem Topkaya <cem.topkaya@telenity.com>
2025-04-25 01:52:30 +0000

    ikinci log satirlari

--- src/components/CounterButton.tsx
+++ src/components/CounterButton.tsx
@@ -7,10 +7,12 @@ export function CounterButton() {
     <button onClick={() => {
       if (count % 2 === 0) {
         setCount(count + 1)
-        console.log("çift")
+        console.log("çift sayi")
+        console.log("çift sayi 1")
       } else {
         setCount(count + 2)
-        console.log("tek")
+        console.log("tek sayi")
+        console.log("tek sayi 1")
       }
     }}>
       count is {count}
